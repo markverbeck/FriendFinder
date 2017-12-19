9cb3643b56d2515c065867e2ce6653f96820801c
Mark Verbeck <scotchhollow@gmail.com>
2017-12-19 10:14:56 -0600

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+var friends = [
+	{
+		name: "Tony Pinchme",
+		photo: "https://i.pinimg.com/736x/2d/dd/6b/2ddd6bc29d0004b7c74bc49027cfc310--lesbian-bisexual.jpg",
+		score: [3,5,2,4,3,3,5,1,2,4]
+	},
+	{
+		name: "Rude Dude",
+		photo: "https://secure.meetupstatic.com/photos/theme_head/9/3/b/b/full_7477819.jpeg",
+		score: [4,1,5,5,3,2,3,4,1,4]
+	},
+	{
+		name: "Wolverine",
+		photo: "https://upload.wikimedia.org/wikipedia/en/c/c8/Marvelwolverine.jpg",
+		score: [4,4,4,5,3,2,4,1,2,3]
+	},
+	{
+		name: "Doug",
+		photo: "http://i47.tinypic.com/2r2rl38.jpg",
+		score: [1,2,4,5,3,3,4,2,2,5]
+	},
+	{
+		name: "Satan",
+		photo: "http://3.bp.blogspot.com/_kKjrNsGlZbE/TQzbIOA8RzI/AAAAAAAAA38/Vp713Ppx1RQ/s320/dd.satan.a.gif",
+		score: [1,1,1,1,1,1,1,1,1,1]
+	},
+	{
+		name: "A Hobo",
+		photo: "https://imgfast.net/users/1313/35/50/68/avatars/25086-21.jpg",
+		score: [2,1,5,5,1,3,2,1,4,1]
+	}
+];
+
+module.exports = friends;
+
